962ccb63876515c18de3b49e9ff7aba73875ed01
Saijal Arora <saijalarora@MacBook-Pro.hitronhub.home>
2022-08-25 00:37:06 -0700

    Initial Commit

---
+++ Desktop/Swift-Assignments/Assignment-3/Problem.swift
@@ -0,0 +1,6 @@
+Design and implement a function which takes 2 operands (numbers) and one operator
+(plus, minus, multiplication and division) and applies the operator on the operands and
+returns and prints the result.
+Note: If operand1 is a non-zero number and the operand2 is zero, then the program
+should not perform the division operand and should print the operation is not possible
+because one number is zero (this is only for division operator) and return -1
